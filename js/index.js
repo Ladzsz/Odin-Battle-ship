07b8dfe3d0dd3fe8358ebeae0ce6ff33f6db34d0
Ladzsz <londonmason602@gail.com>
2025-02-15 10:49:18 -0500

    made gameboard appear

--- js/index.js
+++ js/index.js
@@ -3,9 +3,4 @@ import { Gameboard } from "../js/gameboard.js";
 import { Ship } from "../js/ship.js";
 import { Player } from "../js/player.js";
 
-
-document.getElementById('board').addEventListener("DOMContentLoaded", () => {
-    console.log("Page loaded, creating board...");
-    const game = new Gameboard(10);
-});
-
+const game = new Gameboard(10);
